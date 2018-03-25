f1e6baf28f8e895e8372f6ee7c0947e01e8d2f07
Max Z <max.zlotskiy@gmail.com>
2018-03-25 00:17:44 -0400

    removed x axis labels

--- index.js
+++ index.js
@@ -13,13 +13,39 @@ var data2016 = [
     {name: "Agriculture", amount: 6.68, percent: 1}
 ];
 var data2006 = [
-    //{name: "", amount: , percent: },
+    {name: "National Defense", amount: 552, percent: 51},
+    {name: "Education", amount: 85.6, percent: 8},
+    {name: "", amount: , percent: },
+    {name: "", amount: , percent: },
+    {name: "", amount: , percent: },
+    {name: "", amount: , percent: },
 ];
-var container = d3.select("#container");
-var chart = d3.select("#chart");
-var axisLabelEnter = container.selectAll(".xlabel").data(data2016).enter();
 
-/* CREATE AXIS LABELS */
-d3.select("#yaxis").selectAll(".ylabel").data([600, 500, 400, 300, 200, 100, 0]).enter()
-    .append("span").classed("ylabel", true).style("bottom", function(d) {return d + 'px';}).text(function(d) {return d;});
-axisLabelEnter.append("span").classed("xlabel", true).style("bottom", 0).text(function(d) {return d.name;});
+/* D3 VARIABLES */
+//this div has both the axes and the chart
+var container = d3.select("#container");
+//this div has all the bars inside of it
+var chart = d3.select("#chart");
+//these are the individual bars
+var bars = chart.selectAll(".bar").data(data2016).enter();
+
+/* CREATE Y AXIS LABELS */
+d3.select("#yaxis").selectAll(".ylabel")
+    .data([600, 500, 400, 300, 200, 100, 0]).enter() //the scale on y axis is 100
+    .append("span").classed("ylabel", true) //this class gives them their spacing
+    .text(function(d) {return "$" + d + " billion";}); //this is the display value
+/* CREATE X AXIS LABELS 
+d3.select("#xaxis").selectAll(".xlabel").data(data2016).enter().append("span").classed("xlabel", true)
+    .style("opacity", 0) //make them hidden by default
+    .text(function(d) {return d.name;}); //the name of the budget division 
+
+var categoryLabelsDom = document.getElementsByClassName("xlabel");
+for(var i = 0; i < categoryLabelsDom.length; i++) {
+    categoryLabelsDom[i].addEventListener("mouseenter", function() {
+        this.style.opacity = 1;
+    });
+    categoryLabelsDom[i].addEventListener("mouseleave", function() {
+        this.style.opacity = 0;
+    });
+}*/
+
