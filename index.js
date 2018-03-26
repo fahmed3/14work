6b61bd290d84e60022c212f18638b572a077ea4b
fahmed3 <fahmed3@stuy.edu>
2018-03-26 03:15:20 -0400

    done?

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var b2006 = document.getElementById("fy2006");
+var b2016 = document.getElementById("fy2016");
+var head2 = document.getElementById("head2");
+
 var data2016 = [
     {name: "National Defense", amount: 533, percent: 49},
     {name: "Education", amount: 85.7, percent: 8},
@@ -33,8 +37,34 @@ var container = d3.select("#container");
 //this div has all the bars inside of it
 var chart = d3.select("#chart");
 //these are the individual bars
-var bars = chart.selectAll(".bar").data(data2016).enter();
+var bars = chart.selectAll(".bar").data(data2016);//starts off with 2016 data
 
+var makeBars = function() {
+    if(this.id == "fy2006"){//changes to 2006 data if 2006 button clicked
+	var data = data2006;
+	head2.innerText = "Fiscal Year 2006"
+    }
+    else{//2016 data is default
+	var data = data2016;
+	head2.innerText = "Fiscal Year 2016"
+    }
+    bars.remove();//removes previous bars
+    bars = chart.selectAll(".bar").data(data);
+    bars.enter().append("div").classed("bar", true) //gives them the bar class to make them blue and aligned
+	.transition()
+	.duration(1000)
+	.attr("title", function(d) { //the title attribute gives the element a tooltip when you hover
+            return d.name + " -- $" + d.amount + " billion -- " + d.percent + "%";
+	})
+	.style("height", function(d){ //gives the height of the bar based on amount
+	    return d.amount + "px";
+	})
+	.style("margin-top", function(d){ //adjusts margin-top so graph doesn't look weird
+	    return (600 - d.amount) + "px";
+	});
+}
+
+makeBars();//make the first bars so that bars.remove() doesn't return an error
 
 /* CREATE Y AXIS LABELS */
 d3.select("#yaxis").selectAll(".ylabel")
@@ -48,23 +78,24 @@ var barHoverEvent = function() {
     d3.select("#xaxis").text(text);
 }
 
-/* MAKE BARS */
-bars.append("div").classed("bar", true) //gives them the bar class to make them blue and aligned
-    .attr("title", function(d) { //the title attribute gives the element a tooltip when you hover
-        return d.name + " -- $" + d.amount + " billion -- " + d.percent + "%";
-    });
+
+b2006.addEventListener("click", makeBars);
+b2016.addEventListener("click", makeBars);
+
+
+
 
 /* CREATE X AXIS LABELS 
-d3.select("#xaxis").selectAll(".xlabel").data(data2016).enter().append("span").classed("xlabel", true)
-    .style("opacity", 0) //make them hidden by default
-    .text(function(d) {return d.name;}); //the name of the budget division 
+   d3.select("#xaxis").selectAll(".xlabel").data(data2016).enter().append("span").classed("xlabel", true)
+   .style("opacity", 0) //make them hidden by default
+   .text(function(d) {return d.name;}); //the name of the budget division 
 
-var categoryLabelsDom = document.getElementsByClassName("xlabel");
-for(var i = 0; i < categoryLabelsDom.length; i++) {
-    categoryLabelsDom[i].addEventListener("mouseenter", function() {
-        this.style.opacity = 1;
-    });
-    categoryLabelsDom[i].addEventListener("mouseleave", function() {
-        this.style.opacity = 0;
-    });
-}*/
+   var categoryLabelsDom = document.getElementsByClassName("xlabel");
+   for(var i = 0; i < categoryLabelsDom.length; i++) {
+   categoryLabelsDom[i].addEventListener("mouseenter", function() {
+   this.style.opacity = 1;
+   });
+   categoryLabelsDom[i].addEventListener("mouseleave", function() {
+   this.style.opacity = 0;
+   });
+   }*/
